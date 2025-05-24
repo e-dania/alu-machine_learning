fdbf739d98beef69207d9544c0c0723a034154c0
e-dania <156061632+e-dania@users.noreply.github.com>
2025-05-24 12:22:28 +0200

    Update 2-size_me_please.py

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """
 This module provides a function to determine the shape of a matrix.
-
-
 """
+
+
 def matrix_shape(matrix):
     """
     Returns the shape of a matrix as a list of dimensions.
